798ef5fc92cc10c9091da20ac00c4585a0d981f3
Heitor Farias <heitorfariaspinheirodoamaral@gmail.com>
2025-04-19 21:36:52 +0000

    Pending changes exported from your codespace

---
+++ Aulas/PrimeirosPassos/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿class Program
+{
+    // O ponto de entrada principal para a aplicação.
+    static void Main(string[] args)
+    {
+
+        // Este programa imprime "Hello, World!" na tela.
+        // Console.WriteLine é um método da classe Console que escreve uma linha de texto no console.
+        Console.WriteLine("Hello, World!");
+
+        // O método Console.ReadKey() aguarda o usuário pressionar uma tecla antes de fechar o console.
+        Console.ReadKey();
+    }
+}
